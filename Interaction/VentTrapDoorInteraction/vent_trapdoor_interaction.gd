ea4387d3705835627263854da68ae5caa3d58e5e
Arthur <tutsyyyyy.dpc16@gmail.com>
2026-08-05 22:55:00 -0500

    Resolvido bug de colisão da trapdoor

--- Interaction/VentTrapDoorInteraction/vent_trapdoor_interaction.gd
+++ Interaction/VentTrapDoorInteraction/vent_trapdoor_interaction.gd
@@ -7,10 +7,13 @@ screws holding it shut have been loosened (Global.parafuso_solto). If the
 player tries to interact before that, they get a hint message instead.
 """
 
-## The AnimationPlayer that has the "open" animation for this trapdoor
-@export var animation_player: AnimationPlayer
-
+var animation_player: AnimationPlayer
 var is_open: bool = false
+
+func _ready() -> void:
+	super()
+	# Busca o AnimationPlayer manualmente (irmão desse node, filho do Trapdoor)
+	animation_player = get_parent().get_node("AnimationPlayer")
 
 func interact() -> void:
 	super()
@@ -18,15 +21,18 @@ func interact() -> void:
 	if not can_interact or is_open:
 		return
 
-	print("DEBUG trapdoor: parafuso_solto=", Global.parafuso_solto, " | animation_player=", animation_player)
-
 	if Global.parafuso_solto:
 		is_open = true
 		can_interact = false
 		if animation_player:
 			animation_player.play("open")
-			print("DEBUG trapdoor: animation.play chamado, current_animation=", animation_player.current_animation)
-		else:
-			print("DEBUG trapdoor: animation_player é null!")
+
+		# A caixa de colisão fixa (usada só pra detectar o clique de interação)
+		# não se move com a animação da porta, então continua bloqueando a
+		# passagem física mesmo depois de "aberta". Desativa ela pra deixar
+		# o player passar.
+		var fixed_collision := get_parent().get_node_or_null("CollisionShape3D")
+		if fixed_collision:
+			fixed_collision.disabled = true
 	else:
 		Global.mensagem = "There's locked"
